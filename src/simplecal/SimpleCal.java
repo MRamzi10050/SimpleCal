b4dcee1241345a27b8e012157fe161c3340c0def
MRamzi10050 <58272189+MRamzi10050@users.noreply.github.com>
2021-09-03 14:06:46 +0300

    Add files via upload

---
+++ src/simplecal/SimpleCal.java
@@ -0,0 +1,21 @@
+
+package simplecal;
+
+import java.util.Scanner;
+
+
+public class SimpleCal {
+
+       
+   public static void main(String[] args) {
+
+    int x=0;
+    while(x<10){
+        x++;
+        
+        
+    
+    }
+
+   System.out.println(x);
+}}
